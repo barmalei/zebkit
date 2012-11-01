50c06941738b1a031098ab76fbcdde776f2746b1
vish@gravitysoft.org <vish@gravitysoft.org>
2012-11-01 17:45:41 +0100

    JSON as form descriptive language, many fixes

--- .lithium/lib/gravity/zebra/all.canvas.js
+++ .lithium/lib/gravity/zebra/all.canvas.js
@@ -2,7 +2,6 @@ lib/gravity/zebra/easyoop.js
 lib/gravity/zebra/io.js
 lib/gravity/zebra/util.io.js
 lib/gravity/zebra/util.js
-lib/gravity/zebra/data.js
 lib/gravity/zebra/layout.js
 lib/gravity/zebra/ui.view.js
 lib/gravity/zebra/canvas.js

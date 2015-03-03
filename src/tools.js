73b56edf593ef0a1257ded1bcb2a674cb23053fb
barmalei <vish@gravitysoft.org>
2015-03-03 11:47:20 +0100

    support async test-cases !

--- src/tools.js
+++ src/tools.js
@@ -159,7 +159,7 @@
         }
     ]);
 
-    pkg.$out   = new pkg.Output();
+    pkg.$out  = new pkg.Output();
     pkg.print = function() { pkg.$out.print.apply(pkg.$out, arguments); };
     pkg.error = function() { pkg.$out.error.apply(pkg.$out, arguments); };
     pkg.warn  = function() { pkg.$out.warn.apply(pkg.$out, arguments); };
@@ -299,9 +299,9 @@
     };
 
     pkg.runTests = function() {
-        var pout = pkg.$out, c = 0,  err = 0, sk = 0, title = null;
+        var out = pkg.$out, c = 0,  err = 0, sk = 0, title = null;
         if (pkg.isInBrowser) {
-            pkg.$out = new pkg.HtmlOutput();
+            out = new pkg.HtmlOutput();
         }
 
         var args = Array.prototype.slice.call(arguments);
@@ -309,61 +309,131 @@
             title = args.shift();
         }
 
-        try {
-            pkg.print("Running " + args.length + " test cases "  + (title !== null? "from '" + title + "' test suite" : "") + " :");
-            pkg.print("==============================================");
-            for(var i = 0; i<args.length; i++) {
-                var f = args[i];
-                if (typeof f !== "function") {
-                    throw new Error("Test case has to be function");
+        // validate arguments
+        for(var i = 0; i < args.length; i++) {
+            var f = args[i];
+            if (typeof f !== "function") {
+                throw new Error("Test case has to be function");
+            }
+        }
+
+        out.print("Running " + args.length + " test cases "  + (title !== null? "from '" + title + "' test suite" : "") + " :");
+        out.print("==============================================");
+        if (typeof zebra.util.Runner !== "undefined" && pkg.$useSyncTest != true) {
+            var runner = new zebra.util.Runner();
+
+            for(var i = 0; i < args.length; i++) {
+                var f = args[i], k = pkg.$FN(f);
+
+                if (k.indexOf("_") === 0) {
+                    out.warn("? " + k + " (remove leading '_' to enable '" + k + "' test case)");
+                    sk++;
+                }
+                else {
+                    (function(f, k) {
+                        runner.$currentTestCase = k;
+
+                        runner.assertCallback = function(f) {
+                            var notify = this.join(), $this = this;
+                            return function() {
+                                try {
+                                    runner.$currentTestCase = k;
+                                    f.apply(this, arguments);
+                                    notify.call($this);
+                                }
+                                catch(e) {
+                                    $this.fireError(e);
+                                }
+                            }
+                        };
+
+                        runner.run(function() {
+                            c++ ;
+                            f.call(this);
+                        })
+                        .
+                        run(function() {
+                            out.print("+ " + k);
+                        });
+                    })(f, k);
+                }
+            }
+
+            runner.run(function() {
+                out.print("==============================================");
+                if (c === 0) {
+                    out.warn("No test case to be run was found");
+                }
+                else {
+                    if (sk > 0) {
+                        out.warn("" + sk + " test cases have been skipped");
+                    }
+
+                    out.print((sk === 0 ? "ALL (" + c  + ")" : c) + " test cases have passed successfully");
+                }
+            });
+
+            runner.error(function(e) {
+                if (e instanceof AssertionError) {
+                    out.error("- " + this.$currentTestCase + " || " + e.message);
+                }
+                else {
+                    out.error("" + this.$currentTestCase + " (unexpected error) " + (e.stack ? e.stack : e));
+                    console.log("" + e.stack);
                 }
 
-                var k = pkg.$FN(f);
+                this.$currentTestCase = null;
+            });
+
+        }
+        else {
+            for(var i = 0; i < args.length; i++) {
+                var f = args[i], k = pkg.$FN(f);
+
                 try {
                     if (k.indexOf("_") === 0) {
-                        pkg.warn("? " + k + " (remove leading '_' to enable '" + k + "' test case)");
+                        out.warn("? " + k + " (remove leading '_' to enable '" + k + "' test case)");
                         sk++;
-                        continue;
                     }
-                    c++;
-                    f();
-                    pkg.print("+ " + k);
+                    else {
+                        c++;
+                        f();
+                        out.print("+ " + k);
+                    }
                 }
                 catch(e) {
                     err++;
                     if (e instanceof AssertionError) {
-                        pkg.error("- " + k + " || " + e.message);
+                        out.error("- " + k + " || " + e.message);
                     }
                     else {
-                        pkg.error("" + k + " (unexpected error) " + (e.stack ? e.stack : e));
+                        out.error("" + k + " (unexpected error) " + (e.stack ? e.stack : e));
                         console.log("" + e.stack);
                         throw e;
                     }
                 }
             }
-            pkg.print("==============================================");
+
+            out.print("==============================================");
             if (c === 0) {
-                pkg.warn("No test case to be run was found");
+                out.warn("No test case to be run was found");
             }
             else {
                 if (sk > 0) {
-                    pkg.warn("" + sk + " test cases have been skipped");
+                    out.warn("" + sk + " test cases have been skipped");
                 }
 
                 if (err === 0) {
-                    pkg.print((sk === 0 ? "ALL (" + c  + ")" : c) + " test cases have passed successfully");
+                    out.print((sk === 0 ? "ALL (" + c  + ")" : c) + " test cases have passed successfully");
                 }
                 else {
-                    pkg.error("" + err  + " test cases have failed");
+                    out.error("" + err  + " test cases have failed");
                 }
 
                 if (err > 0) {
                     throw new Error("" + err + " test case(s) have failed");
                 }
             }
-        }
-        finally {
-            pkg.$out = pout;
         }
     };
 
